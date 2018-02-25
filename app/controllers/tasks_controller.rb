9dfb4daf75f9ba3a2f0d08afb4db8e47af83f243
Ryan Marks <Ryan@paleo2go@gmail.com>
2018-02-25 22:35:21 +0000

    Update Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
-class TasksController < ApplicationController
-  def index
-    render json: Task.all
+class TasksController < ApplicationController #TasksController inherits all traits from Application Controller 
+  def index #define index method
+    render json: Task.all #JSON representation of all Tasks 
+  end #end of index method 
+
+  def update
+    task = Task.find(params[:id])#find the task by it's id
+    task.update_attributes(task_params)#updates task to show it's done
+    render json: task #JSON representation of updated item 
+  end 
+
+  private #private method 
+
+  def task_params #naming method task_params
+    params.require(:task).permit(:done)
   end
+
 end
