089e116f4bf93a61a650345a99fcc88785ec4bd5
Ryan Marks <Ryan@paleo2go@gmail.com>
2018-02-26 16:14:37 +0000

    strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -7,8 +7,10 @@
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+        '<div class="view"><input class="toggle" type="checkbox"' + 
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -34,7 +36,14 @@
           task: {
             done: doneValue
           }
-        });
+        }).success(function(data) {
+          var liHtml = taskHtml(data);
+          var $li = $("#listItem-" + data.id);
+          $li.replaceWith(liHtml);
+          $('.toggle').change(toggleTask);
+
+          
+        } );
 
       }
 
